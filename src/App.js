fc0b2c3f470be0636ec07030e0498913f96c958f
Jamgoori <lks2354@naver.com>
2023-01-31 15:04:54 +0900

    Navbar, 다크모드 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar";
 import React from "react";
+import { ThemeProvider } from "./context/ThemeContext";
 function App() {
   return (
-    <div>
+    <ThemeProvider>
       <Navbar />
-    </div>
+    </ThemeProvider>
   );
 }
 
