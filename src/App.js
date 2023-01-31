956e38b43aabf97f21d16dd16c067bc735127f44
Jamgoori <lks2354@naver.com>
2023-01-31 14:41:28 +0900

    기본세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+import React from "react";
 function App() {
-  return <div>Helㄴlo</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
