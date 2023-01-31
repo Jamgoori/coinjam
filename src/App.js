1e8d9d7a074009b6b71eba7facd0fc572022225a
Jamgoori <lks2354@naver.com>
2023-01-31 16:32:09 +0900

    axios 연결

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import Navbar from "./components/Navbar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "./context/ThemeContext";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import axios from "axios";
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=krw&order=market_cap_desc&per_page=20&page=1&sparkline=true`;
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      console.log(response.data);
+    });
+  }, [url]);
+
   return (
     <ThemeProvider>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </ThemeProvider>
   );
 }
