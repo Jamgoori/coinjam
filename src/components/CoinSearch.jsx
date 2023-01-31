992d90bc09ede752bc34a87690b1823c700ba83e
Jamgoori <lks2354@naver.com>
2023-01-31 17:05:06 +0900

    데이터 불러오기

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
+import { Sparklines, SparklinesLine } from "react-sparklines";
+const CoinSearch = ({ coins }) => {
+  console.log(coins);
+  return (
+    <div>
+      <div>
+        <h1>코인 검색</h1>
+        <form>
+          <input type="text" placeholder="코인 검색" />
+        </form>
+      </div>
 
-const CoinSearch = () => {
-  return <div>CoinSearch</div>;
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th>coin</th>
+            <th></th>
+            <th>가격</th>
+            <th>24시간</th>
+            <th>24시간 거래량</th>
+            <th>현재가</th>
+            <th>7일 전</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr>
+              <td>
+                <AiOutlineStar />
+              </td>
+              <td>{coin.market_cap_rank}</td>
+              <td>
+                <div>
+                  <img src={coin.image} alt={coin.id} />
+                  <p>{coin.name}</p>
+                </div>
+              </td>
+              <td>{coin.symbol}</td>
+              <td>{coin.current_price}</td>
+              <td>{coin.price_change_percentage_24h}</td>
+              <td>{coin.total_volume}</td>
+              <td>{coin.market_cap}</td>
+              <td>
+                <Sparklines data={coin.sparkline_in_7d.price}>
+                  <SparklinesLine color="teal" />
+                </Sparklines>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default CoinSearch;
