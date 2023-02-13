b07dd0f5fb1b8de2f830ec67ce5d40135e760734
Jamgoori <lks2354@naver.com>
2023-02-13 10:59:14 +0900

    prettier 설정 추가

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import Navbar from "./components/Navbar";
-import React, { useEffect, useState } from "react";
-import { ThemeProvider } from "./context/ThemeContext";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Signin from "./pages/Signin";
-import Signup from "./pages/Signup";
-import Account from "./pages/Account";
-import Coinpage from "./pages/Coinpage";
-import axios from "axios";
-import Footer from "./components/Footer";
-import { AuthContextProvider } from "./context/AuthContext";
+import Navbar from './components/Navbar'
+import React, { useEffect, useState } from 'react'
+import { ThemeProvider } from './context/ThemeContext'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Account from './pages/Account'
+import Coinpage from './pages/Coinpage'
+import axios from 'axios'
+import Footer from './components/Footer'
+import { AuthContextProvider } from './context/AuthContext'
 function App() {
-  const [coins, setCoins] = useState([]);
-
-  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=krw&order=market_cap_desc&per_page=20&page=1&sparkline=true`;
+  const [coins, setCoins] = useState([])
+  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=krw&order=market_cap_desc&per_page=20&page=1&sparkline=true`
 
   useEffect(() => {
     axios
       .get(url)
       .then((response) => {
-        setCoins(response.data);
-        console.log(response.data);
+        setCoins(response.data)
+        console.log(response.data)
       })
       .catch(() => {
-        alert("코인게코의 서버가 불안정합니다.");
-      });
-  }, [url]);
+        alert('코인게코의 서버가 불안정합니다.')
+      })
+  }, [url])
 
   return (
     <ThemeProvider>
@@ -43,7 +42,7 @@ function App() {
         <Footer />
       </AuthContextProvider>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import CoinSearch from "../components/CoinSearch";
 import GoToTop from "../components/GotoTop";
 import Trending from "../components/Trending";
-
 const Home = ({ coins }) => {
   return (
     <div>
