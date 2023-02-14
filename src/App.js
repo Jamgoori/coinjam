264c449e3cd70a04d0cbaa4e8ab6f789ee22409d
Jamgoori <lks2354@naver.com>
2023-02-14 09:11:56 +0900

    처음 진입시, 프로젝트 전체에서 유저 정보 확인

--- src/App.js
+++ src/App.js
@@ -8,11 +8,32 @@ import Signup from './pages/Signup'
 import Account from './pages/Account'
 import Coinpage from './pages/Coinpage'
 import Footer from './components/template/Footer'
-import { AuthContextProvider } from './context/AuthContext'
 import { getCoins } from './queries/getCoins'
+import { useDispatch } from 'react-redux'
+import { login, logout } from './store/authStore'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
 
 function App() {
+  const dispatch = useDispatch()
   const [coins, setCoins] = useState([])
+
+  // page load 때 user 체크
+  useEffect(() => {
+    onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        // 유저가 존재하면 login dispatch
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+          })
+        )
+      } else {
+        dispatch(logout())
+      }
+    })
+  }, [])
 
   useEffect(() => {
     getCoins()
@@ -27,21 +48,18 @@ function App() {
 
   return (
     <ThemeProvider>
-      <AuthContextProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home coins={coins} />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/coin/:coinId" element={<Coinpage />}>
-            <Route path=":coinId" />
-          </Route>
-        </Routes>
-        <Footer />
-      </AuthContextProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home coins={coins} />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/coin/:coinId" element={<Coinpage />}>
+          <Route path=":coinId" />
+        </Route>
+      </Routes>
+      <Footer />
     </ThemeProvider>
   )
 }
-
 export default App
